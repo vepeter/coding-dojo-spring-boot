200062235ecc49b070622a532a356c7da3c44bf3
Peter Velychko <peter.velychko@gmail.com>
2021-02-08 01:13:52 +0100

    added integration test

--- src/main/java/com/assignment/spring/Constants.java
+++ src/main/java/com/assignment/spring/Constants.java
@@ -1,8 +1,12 @@
 package com.assignment.spring;
 
 public class Constants {
+    
+    private Constants() {
+        // noop
+    }
 
-    public static final String APP_ID = "TBD";
+    public static final String APP_ID = "136acafec97e6cf79baf9bff29604560";
 
     public static final String WEATHER_API_URL = "http://api.openweathermap.org/data/2.5/weather?q={city}&APPID={appid}";
 }
---
+++ src/test/java/com/assignment/spring/ApplicationIntegrationTest.java
@@ -0,0 +1,51 @@
+package com.assignment.spring;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.servlet.context.ServletWebServerApplicationContext;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import com.assignment.spring.api.WeatherResponse;
+
+class ApplicationIntegrationTest extends AbstractIntegrationTest {
+
+    @Autowired
+    private ServletWebServerApplicationContext webServerApplicationContext;
+    
+    @Autowired
+    private WeatherRepository weatherRepository;
+
+    private TestRestTemplate testRestTemplate = new TestRestTemplate();
+
+    private String cityDefinition = "Amsterdam,nl";
+
+    @Test
+    void test() {
+        Double expectedTemperature = loadExpectedTemperature();
+        String applicationUrl = String.format("http://localhost:%s/weather?city=%s",
+                webServerApplicationContext.getWebServer().getPort(), cityDefinition);
+        
+        ResponseEntity<WeatherEntity> response = testRestTemplate.getForEntity(applicationUrl, WeatherEntity.class);
+        
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(response.getBody())
+                .extracting("city", "country", "temperature")
+                .containsExactly("Amsterdam", "NL", expectedTemperature);
+        
+        assertThat(weatherRepository.findAll()).hasSize(1);
+        assertThat(weatherRepository.findById(1).get())
+                .extracting("city", "country", "temperature")
+                .containsExactly("Amsterdam", "NL", expectedTemperature);
+    }
+
+    private Double loadExpectedTemperature() {
+        String apiUrl = String.format("http://api.openweathermap.org/data/2.5/weather?q=%s&APPID=%s",
+                cityDefinition, Constants.APP_ID);
+        ResponseEntity<WeatherResponse> apiResponse = testRestTemplate.getForEntity(apiUrl, WeatherResponse.class);
+        return apiResponse.getBody().getMain().getTemp();
+    }
+}
